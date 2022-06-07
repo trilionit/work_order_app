ffff8384be6529e6bdf231d7e2b3eeff6cf6ef9d
Prince Osei-Akyeampong <prince@trilionit.com>
2022-06-06 18:35:22 -0700

    Added frontend framework with initial functionality

--- views/pages/home/components/header.php
+++ views/pages/home/components/header.php
@@ -1,4 +1,4 @@
-<p class="h1 text-center mt-3 mb-4 pb-3 text-primary">
-  <i class="fas fa-check-square me-1"></i>
-  <u>Work Order</u>
-</p>
+<?php
+include_once __DIR__.'/title.html';
+include_once __DIR__.'/add.html';
+include_once __DIR__.'/filter-sort.html';
