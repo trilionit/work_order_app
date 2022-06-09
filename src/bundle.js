20f79bdcbba3b4bec3e77639fbc4d8f4f26e7f51
Prince Osei-Akyeampong <prince@trilionit.com>
2022-06-08 17:54:00 -0700

    added datetime picker

--- src/bundle.js
+++ src/bundle.js
@@ -4,15 +4,17 @@ if (typeof setDueDateButton !== "undefined" && setDueDateButton !== null) {
   setDueDateButton.addEventListener("click", (e) => {
     e.preventDefault();
     const targetID = e.target.getAttribute("data-id");
-    const containerID = document.getElementById("new-component");
-    let div = document.createElement("div");
-    let classesToAdd = ["col"];
-    div.classList.add(...classesToAdd);
-    div.setAttribute("id", "datepicker");
-    let datepickerCtn = document.createElement("input");
-    datepickerCtn.classList;
-    div.appendChild("In the beginning");
-    containerID.appendChild(div);
-    console.log(div);
+    const showComponent = document.getElementById(`${targetID}`);
+    const datePicker = document.getElementById("datepicker");
+    const validate_class = "validate-input";
+
+    showComponent.style.display =
+      showComponent.style.display === "block" ? "none" : "block";
+
+    showComponent.style.display === "block"
+      ? datePicker.classList.add(validate_class)
+      : datePicker.classList.remove(validate_class);
   });
 }
+// Datetime picker
+new tempusDominus.TempusDominus(document.getElementById("datetime"));
